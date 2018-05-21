26bfe4cc5be3c29c01b20b34fcc361e1784bcf96
ChernetsovNG <n.chernetsov86@gmail.com>
2018-05-21 19:01:34 +0300

    SberTech invoke-remote-app: рефакторинг (удаление лишнего из Message)

--- SberTech_RemoteInvoke/common/src/main/java/ru/nchernetsov/test/sbertech/common/message/Message.java
+++ SberTech_RemoteInvoke/common/src/main/java/ru/nchernetsov/test/sbertech/common/message/Message.java
@@ -2,8 +2,6 @@ package ru.nchernetsov.test.sbertech.common.message;
 
 import lombok.Getter;
 import lombok.ToString;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
 import java.util.UUID;
@@ -14,9 +12,6 @@ import java.util.UUID;
 @Getter
 @ToString
 public abstract class Message implements Serializable {
-    public static final Logger LOG = LoggerFactory.getLogger(Message.class);
-    public static final String CLASS_NAME_VARIABLE = "className";
-
     private final UUID uuid;        // уникальный идентификатор сообщения
     private final Address from;
     private final Address to;
