e076c48e602c4e855a591dd669ac02a0ee8957f8
ChernetsovNG <n.chernetsov86@gmail.com>
2019-04-14 14:50:58 +0300

    test-yandex: doDigest method refactor

--- Yandex/src/main/java/ru/nchernetsov/test/yandex/test3/badDigest/ConcreteDigestSum.java
+++ Yandex/src/main/java/ru/nchernetsov/test/yandex/test3/badDigest/ConcreteDigestSum.java
@@ -4,12 +4,10 @@ public class ConcreteDigestSum extends Digest {
 
     @Override
     protected byte[] doDigest(byte[] input) {
-        byte[] output = new byte[1];
         byte sum = 0;
         for (byte b : input) {
             sum += b;
         }
-        output[0] = sum;
-        return output;
+        return new byte[]{sum};
     }
 }
--- Yandex/src/main/java/ru/nchernetsov/test/yandex/test3/notSoBadDigest/ConcreteDigestSum.java
+++ Yandex/src/main/java/ru/nchernetsov/test/yandex/test3/notSoBadDigest/ConcreteDigestSum.java
@@ -4,12 +4,10 @@ public class ConcreteDigestSum implements DigestWorkerService {
 
     @Override
     public byte[] doDigest(byte[] input) {
-        byte[] output = new byte[1];
         byte sum = 0;
         for (byte b : input) {
             sum += b;
         }
-        output[0] = sum;
-        return output;
+        return new byte[]{sum};
     }
 }
