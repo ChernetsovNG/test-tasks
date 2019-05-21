028360c6298345e29646d528ea57303b1678755b
nchernetsov <n.chernetsov86@gmail.com>
2019-05-21 20:24:00 +0300

    LIPT: init commit

---
+++ LIPT/src/main/java/ru/nchernetsov/lipt/LiptTestApplication.java
@@ -0,0 +1,13 @@
+package ru.nchernetsov.lipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiptTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiptTestApplication.class, args);
+    }
+
+}
